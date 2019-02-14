bf1761fa19f919ea9a2dd42b470daa5c54beaeb0
hy2850 <39735858+hy2850@users.noreply.github.com>
2019-02-14 15:16:53 +0900

    [PHP] Automated topic list generation
    Used scandir to read file names in the directory and while-loop to generate list.

--- php/index.php
+++ php/index.php
@@ -6,11 +6,26 @@
   </head>
   <body>
     <h1><a href="index.php">WEB</a></h1>
-    <ol>
-      <li><a href="index.php?id=HTML">HTML</a></li>
-      <li><a href="index.php?id=CSS">CSS</a></li>
-      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
-    </ol>
+
+    <?php
+      // Making list of topics in the 'data' folder
+      $files = array_slice(scandir("./data", 0), 2); //'scandir' function includes two redundant addresses, '.' and '..'
+      $list = "<ol>";
+
+      $i=0;
+      while($i<count($files)){
+        $list .= "<li><a href=\"index.php?id=$files[$i]\">$files[$i]</a></li>\n";
+
+        // Alternatives : 
+        // "<li><a href='index.php?id=$files[$i]'>$files[$i]</a></li>\n"
+        // '<li><a href="index.php?id='.$files[$i].'">'.$files[$i].'</a></li>';
+
+        $i += 1;
+      }
+      $list .= "</ol>";
+
+      echo $list;
+    ?>
 
     <h2>
     	<?php 
